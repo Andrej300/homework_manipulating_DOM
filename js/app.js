162e080d8f978511ef2a55891223d8887a968414
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-01-10 21:02:22 +0000

    html doc

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const newItemform = document.querySelector('#new-item-form');
+    newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('clic', handleDeleteAllClick);
+    
+})
+
