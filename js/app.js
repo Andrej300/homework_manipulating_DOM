bdf1cf4cd90ac04fadf2f11808efca2318f78e67
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-01-11 12:50:25 +0000

    fixed errors

--- js/app.js
+++ js/app.js
@@ -19,20 +19,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const createPlayerListItem = function (form) {
     const playerListItem = document.createElement('li');
     playerListItem.classList.add('playing-list-item');
+    
+    // const first = document.createElement('h2');
+    // first.textContent = form.first.value;
+    // playerListItem.appendChild(first);
   
-    const position = document.createElement('h2');
+    // const last = document.createElement('h2');
+    // last.textContent = form.last.value;
+    // playerListItem.appendChild(last);
+
+    const fullName = document.createElement('h2');
+    fullName.textContent = `${form.first.value} ${form.last.value}`;
+    playerListItem.appendChild(fullName);
+
+    const position = document.createElement('h3');
     position.textContent = form.position.value;
-    palayerListItem.appendChild(position);
+    playerListItem.appendChild(position);
   
     const country = document.createElement('h3');
-    county.textContent = form.country.value;
+    country.textContent = form.country.value;
     playerListItem.appendChild(country);
+    
   
-    const first = document.createElement('p');
-    category.textContent = form.first.value;
-    playerListItem.appendChild(first);
 
-    console.log(form.stillPlaying.value);
+    // console.log(form.stillPlaying.value);
   
     return playerListItem;
   }
